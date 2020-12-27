751232d6af05c1503430d0c35c916558e53e908d
onlymr <a94122@alunos.uminho.pt>
2020-12-27 20:28:16 +0000

    Andre

--- BackEnd/controllers/participations.controller.js
+++ BackEnd/controllers/participations.controller.js
@@ -3,10 +3,10 @@ const connect = require('../config/connect.js');
 const jsonMessages = require("../assets/jsonMessages/bd");
 
 //Saber os suspeitos de determinada ocorrencia
-function getSuspects(req,res) {
-    const query = connect.con.query('SELECT * FROM Suspect s , Occurrence o  order by id_suspect', function(err, rows, fields){
+function getSuspectsInOccurrence(req,res) {
+    const query = connect.con.query('SELECT o.id_occurrence, s.* FROM Occurrence o, Suspect s order by s.id_suspect WHERE o.id_occurrence IN (SELECT id_occurrence FROM Participation) AND s.id_suspect IN (SELECT id_suspect FROM Participation) AND s.active=1', function(err, rows, fields){
         console.log(query.sql);
-    
+        
         if(err) {
             console.log(err);
             res.status(jsonMessages.db.dbError.status).send(jsonMessages.db.dbError);
