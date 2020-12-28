973017d47ef6e8899cb921b77722c9f7f0501d6e
dagama14 <73606475+dagama14@users.noreply.github.com>
2020-12-28 14:25:03 +0000

    Vasco

--- FrontEnd/startbootstrap-sb-admin-gh-pages/dist/js/fetchDadosSuspeitos.js
+++ FrontEnd/startbootstrap-sb-admin-gh-pages/dist/js/fetchDadosSuspeitos.js
@@ -3,14 +3,26 @@ function refreshSuspect() {
     let id_suspect = document.getElementById("idSuspeito");
     let id = localStorage.getItem("id_suspect");
     let name = document.getElementById("nomeSuspeito");
+    let naturality = document.getElementById("naturalidadeSuspeito");
+    let genre = document.getElementById("sexoSuspeito");
+    let cc_number = document.getElementById("ccSuspeito");
+    let job = document.getElementById("profissaoSuspeito");
+    let skin_color = document.getElementById("peleSuspeito");
+    let eyes_color = document.getElementById("olhosSuspeito");
+    let hair_color = document.getElementById("cabeloSuspeito");
+    let height = document.getElementById("alturaSuspeito");
+    let body_shape = document.getElementById("corpoSuspeito");
+
     const renderUsers = document.getElementById("result");
     const response = await fetch(`http://localhost:3000/Suspects/${id}`);
     const users = await response.json();
 
     id_suspect.setAttribute("value", users[0].id_suspect);
     name.setAttribute("value", users[0].name);
+
     console.log(users[0].id_suspect);
     console.log(users[0].name);
+    
   }
   //chama a função fetchAsync()
   fetchAsync()
