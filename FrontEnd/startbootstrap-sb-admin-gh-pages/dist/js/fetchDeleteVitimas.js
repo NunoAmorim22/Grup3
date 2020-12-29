c7222ab65d1add43f69a05ac95d04e7950a79662
dagama14 <73606475+dagama14@users.noreply.github.com>
2020-12-29 15:48:29 +0000

    sweet alert com confirm

--- FrontEnd/startbootstrap-sb-admin-gh-pages/dist/js/fetchDeleteVitimas.js
+++ FrontEnd/startbootstrap-sb-admin-gh-pages/dist/js/fetchDeleteVitimas.js
@@ -1,24 +1,36 @@
 function DeleteVictim() {
     var data = {};
-    data.id_victim= document.getElementById("idVitima").value;
+    data.id_victim = document.getElementById("idVitima").value;
     console.log(data); //debugging para ver os dados que foram enviados
     //chamada fetch para envio dos dados para o servior via POST
     fetch(`http://localhost:3000/participations/deleteVictims/${data.id_victim}`, {
-    headers: {'Content-Type': 'application/json'},
-    method: 'DELETE',
-    body: JSON.stringify(data)
+        headers: { 'Content-Type': 'application/json' },
+        method: 'DELETE',
+        body: JSON.stringify(data)
     }).then(function (response) {
-    if (!response.ok) {
-    console.log(response.status); //=> number 100–599
-    console.log(response.statusText); //=> String
-    console.log(response.headers); //=> Headers
-    } else {
-    Swal.fire("Vitima Apagada");
-   console.log("Success DELETE");
-   console.log(response);
-   document.location.href="./ListaVitimas.html";
-    }
-    }).then(function (result) {console.log(result);
-    }).catch(function (err) {alert("Submission error"); console.error(err);
+        if (!response.ok) {
+            console.log(response.status); //=> number 100–599
+            console.log(response.statusText); //=> String
+            console.log(response.headers); //=> Headers
+        } else {
+            //Swal.fire("Vitima Apagada");
+            sweetAlert({
+                title: 'Warning!',
+                text: 'Vitima apagada!',
+                type: 'warning'
+            }, function (isConfirm) {
+                alert('ok');
+            });
+            $('.swal2-confirm').click(function () {
+                window.location.href = './ListaVitimas.html';
+            });
+            console.log("Success DELETE");
+            console.log(response);
+            //document.location.href="./ListaVitimas.html";
+        }
+    }).then(function (result) {
+        console.log(result);
+    }).catch(function (err) {
+        alert("Submission error"); console.error(err);
     });
-    }
+}
