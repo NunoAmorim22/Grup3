c9d54071a15e690cb70e7ad9d31745612d54ff46
tiagocarvalho0704 <a92978@alunos.uminho.pt>
2020-12-30 23:02:52 +0000

    69

--- BackEnd/routes/participations.route.js
+++ BackEnd/routes/participations.route.js
@@ -19,11 +19,7 @@ router.delete("/deletewits/:id",participationsController.deleteTestemunhaF);
 router.delete("/deleteVictims/:id",participationsController.deleteVitimaF);
 router.delete("/deletesuspects/:id",participationsController.deleteSuspectF);
 router.post("/occurrences/:id_occu",participationsController.postSuspect);
-/*
-router.post('/postParticipants',participantController.postParticipants);
-router.put("/del/:id",participantController.deleteParticipantsL);
-router.delete("/:id",participantController.deleteParticipantsF);
-router.put("/:id",participantController.updatePartcipants);*/
+router.post("/occurrence/:id",participationsController.postParticipant);
 
 //No final do DOC
 module.exports = router ;
