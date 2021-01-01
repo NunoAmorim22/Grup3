b00234956996fab8a1e66b63fc16c43b1e6608b9
onlymr <a94122@alunos.uminho.pt>
2021-01-01 18:01:45 +0000

    andremonster

--- BackEnd/routes/occurrences.route.js
+++ BackEnd/routes/occurrences.route.js
@@ -9,8 +9,8 @@ router.get('/', function(req, res) {
 
 router.get("/allActive",occurrencesController.getAllActiveOccurrences);
 router.get("/allEnded",occurrencesController.getAllEndedOccurrences);
-router.get("/allPart",occurrencesController.getAllParticipations);
-router.get("/allPres",occurrencesController.getAllPresences);
+router.get("/allPart/:id",occurrencesController.getAllParticipations);
+router.get("/allPres/:id",occurrencesController.getAllPresences);
 
 //No final do DOC
 module.exports = router ;
