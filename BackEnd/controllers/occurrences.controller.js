76417863a225d5789c25f6740374687b44f157f6
onlymr <a94122@alunos.uminho.pt>
2021-01-06 14:33:23 +0000

    andremosnter

--- BackEnd/controllers/occurrences.controller.js
+++ BackEnd/controllers/occurrences.controller.js
@@ -33,7 +33,7 @@ function getAllActiveTeamOccurrences(req,res){
     const idOperational= req.params.id;
     update =[idOperational,"Em Processo"];
 
-    const query =connect.con.query ("SELECT  DISTINCT o.id_occurrence FROM Occurrence o, Team_Inscription ti WHERE o.id_team=ti.id_team AND ti.id_team IN (SELECT t.id_team FROM Team_Inscription ti, Operational op, Team t, Occurrence o WHERE op.id_operational=ti.id_operational AND ti.id_team=t.id_team AND op.id_operational=?)", update, function(err, rows, fields){
+    const query =connect.con.query ("SELECT  DISTINCT o.id_occurrence FROM Occurrence o, Team_Inscription ti WHERE o.id_team=ti.id_team AND ti.id_team IN (SELECT t.id_team FROM Team_Inscription ti, Operational op, Team t, Occurrence o WHERE op.id_operational=ti.id_operational AND ti.id_team=t.id_team AND op.id_operational=? AND o.state=?)", update, function(err, rows, fields){
         console.log(query.sql);
     
         if(err) {
