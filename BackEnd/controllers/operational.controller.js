2328c766f7762cdfe30808768873f398f42a44d8
onlymr <a94122@alunos.uminho.pt>
2021-01-06 16:30:16 +0000

    andremonster

--- BackEnd/controllers/operational.controller.js
+++ BackEnd/controllers/operational.controller.js
@@ -50,13 +50,14 @@ function deleteOperationals (req , res) {
 
 //get dos dados de operacional para o perfil
 //levar id de operacional
-//aparecer Email,id,nome,equipa e creditos
+// vai buscar a equipa
 
-function getOprationalData(req,res){
-    
-    
 
-    const query =connect.con.query ("SELECT o.id_operational, c.name FROM Operational o, Candidate c WHERE o.id_candidate=c.id_candidate", function(err, rows, fields){
+function getOprationalDataTeam(req,res){
+    
+    idOperational=req.params.id;
+
+    const query =connect.con.query ("SELECT   DISTINCT  t.team_indicative FROM Team_Inscription ti, Team t WHERE t.id_team=ti.id_team AND ti.id_team IN (SELECT t.id_team FROM Team_Inscription ti, Operational op, Team t, Occurrence o WHERE op.id_operational=ti.id_operational AND ti.id_team=t.id_team AND op.id_operational=?)",idOperational, function(err, rows, fields){
         console.log(query.sql);
     
         if(err) {
@@ -74,9 +75,46 @@ function getOprationalData(req,res){
     
     });
 }
+
+
+
+
+//aparecer Email,id,nome,equipa e creditos
+function getOprationalDataRest(req,res){
+    
+    idOperational=req.params.id;
+
+    const query =connect.con.query ("SSELECT c.email, op.id_operational, c.name, op.credits FROM Candidate c, Operational op WHERE op.id_candidate=c.id_candidate AND op.id_operational =?;",idOperational, function(err, rows, fields){
+        console.log(query.sql);
+    
+        if(err) {
+            console.log(err);
+            res.status(jsonMessages.db.dbError.status).send(jsonMessages.db.dbError);
+                }
+                else{
+                    if(rows.length==0) {
+                        res.status(jsonMessages.db.noRecords.status).send(jsonMessages.db.noRecords);
+                    }
+                    else{
+                        res.send(rows);
+                    }
+                }
+    
+    });
+}
+
+
+
+
+
+
+
+
+
 module.exports = {
     getAllOperationals:getAllOperationals,
     deleteOperationals:deleteOperationals,
-    getOprationalData:getOprationalData 
+    getOprationalDataTeam:getOprationalDataTeam,
+    getOprationalDataRest:getOprationalDataRest
 
 };
