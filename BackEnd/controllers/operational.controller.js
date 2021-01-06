a9d5edfb3f2fb7f1b0102feca994f0495bf6e710
onlymr <a94122@alunos.uminho.pt>
2021-01-06 15:31:51 +0000

    adnremosnter

--- BackEnd/controllers/operational.controller.js
+++ BackEnd/controllers/operational.controller.js
@@ -48,8 +48,34 @@ function deleteOperationals (req , res) {
     }
 }
 
+//get dos dados de operacional para o perfil
+//levar id de operacional
+
+function getOprationalData(req,res){
+    
+    
+
+    const query =connect.con.query ("SELECT o.id_operational, c.name FROM Operational o, Candidate c WHERE o.id_candidate=c.id_candidate", function(err, rows, fields){
+        console.log(query.sql);
+    
+        if(err) {
+            console.log(err);
+            res.status(jsonMessages.db.dbError.status).send(jsonMessages.db.dbError);
+                }
+                else{
+                    if(rows.length==0) {
+                        res.status(jsonMessages.db.noRecords.status).send(jsonMessages.db.noRecords);
+                    }
+                    else{
+                        res.send(rows);
+                    }
+                }
+    
+    });
+}
 module.exports = {
     getAllOperationals:getAllOperationals,
-    deleteOperationals:deleteOperationals 
+    deleteOperationals:deleteOperationals,
+    getOprationalData:getOprationalData 
 
 };
