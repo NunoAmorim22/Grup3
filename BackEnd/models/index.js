5e0127628ea0e04f025536111b21b200de98fb08
onlymr <a94122@alunos.uminho.pt>
2021-01-13 20:34:18 +0000

    aa

--- BackEnd/models/index.js
+++ BackEnd/models/index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 
-const env = "MySQL";
+const env = process.env.NODE_ENV || "MySQL";
 const config = require ("../config/config.json") [env];
 const sequelize = new Sequelize (config.database, config.username, config.password);
 
