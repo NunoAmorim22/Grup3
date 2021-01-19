dec8dbfd6bf5250db2ee843fa33bf810c9951d76
NunoAmorim22 <nuno22amorim@gmail.com>
2021-01-19 11:14:45 +0000

    troquei o app por router

--- BackEnd/routes/auth.route.js
+++ BackEnd/routes/auth.route.js
@@ -3,28 +3,21 @@ const router = require("express").Router();
 const passport = require("passport");
 const authController = require("../controllers/auth.controller.js");
 
-module.exports = function (app, passport) {
+module.exports = function (router, passport) {
   router.get("/signup", authController.signup);
   router.get("/signin", authController.signin);
   router.get("/signupSuccess", authController.signupSuccess);
   router.get("/signinSuccess", isLoggedIn, authController.signinSuccess);
-
-  router.post(
-    "/signup",
-    passport.authenticate("local-signup", {
+  router.post("/signup",passport.authenticate("local-signup", {
       successRedirect: "/signupSuccess",
       failureRedirect: "/signup",
-    })
-  );
+    }));
   router.get("/logout", authController.logout);
-
-  router.post(
-    "/signin",
-    passport.authenticate("local-signin", {
+  router.post("/signin",passport.authenticate("local-signin", {
       successRedirect: "/signinSuccess",
       failureRedirect: "/signin",
-    })
-  );
+    }));
+
   function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) return next();
     res.redirect("/signin");
