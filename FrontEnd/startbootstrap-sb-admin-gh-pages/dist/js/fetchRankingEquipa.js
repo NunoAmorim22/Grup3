fae48f26588b371976e2a8092b32f294bef10371
dagama14 <73606475+dagama14@users.noreply.github.com>
2021-01-20 13:10:17 +0000

    afinal o tamanho importa!! que aquilo cresce

--- FrontEnd/startbootstrap-sb-admin-gh-pages/dist/js/fetchRankingEquipa.js
+++ FrontEnd/startbootstrap-sb-admin-gh-pages/dist/js/fetchRankingEquipa.js
@@ -1,7 +1,7 @@
-const api_url = `http://localhost:3000/occurrences/rankings/team`;
+const api_url1 = `http://localhost:3000/rankings/teams`;
 
 // Defining async function
-async function getapi(url) {
+async function getapi1(url) {
   // Storing response
   const response = await fetch(url);
 
@@ -9,26 +9,26 @@ async function getapi(url) {
   var data = await response.json();
   console.log(data);
   if (response) {
-    hideloader();
+    hideloader1();
   }
   show(data); 
 }
 // Calling that async function
-getapi(api_url);
+getapi1(api_url1);
 // Function to hide the loader
-function hideloader() {
-  document.getElementById("loading").style.display = "none";
+function hideloader1() {
+  document.getElementById("loading1").style.display = "none";
 }
 
 // Function to define innerHTML for HTML table
 function show(data) {
-  let tab = `<tr><th>ID Ocorrência</th></tr>`;
+  let tab = `<tr><th>Indicativo</th> <th>Créditos</th></tr>`;
 
   // Loop to access all rows
   for (let r of data) {
-    tab += `<tr><td>${r.id_occurrence} </td> <td><a class="btn btn-link" name="irpara" onclick = "transportidOccurrence(${r.id_occurrence})"> <i class="fas fa-check"></i> </a></td></tr>`;
+    tab += `<tr><td>${r.team_indicative} </td> <td>${r.total_credits}</td></tr>`;
   
   }
   // Setting innerHTML as tab variable
-  document.getElementById("lista-ocorrencias").innerHTML = tab;
+  document.getElementById("ranking-team").innerHTML = tab;
 }
