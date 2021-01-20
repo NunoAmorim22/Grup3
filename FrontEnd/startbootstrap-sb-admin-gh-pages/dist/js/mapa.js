56157a00311eed204b1b47ed55863be28e31cf64
NunoAmorim22 <nuno22amorim@gmail.com>
2021-01-20 17:08:21 +0000

    mapa com market

--- FrontEnd/startbootstrap-sb-admin-gh-pages/dist/js/mapa.js
+++ FrontEnd/startbootstrap-sb-admin-gh-pages/dist/js/mapa.js
@@ -1,11 +1,44 @@
-let map;
+/*let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 41.14961, lng: -8.61099},
     zoom: 10,
-  });
-
-
-  
+  });  
 }
+*/
+function initMap() {
+
+  // Ponto no mapa a localizar (cidade do Porto)
+  const porto = new google.maps.LatLng(41.14961  , -8.61099)
+
+  // Propriedades do mapa
+  const mapProp = {
+    center:porto, 
+    zoom:12, 
+    scrollwheel:false, 
+    draggable:false, 
+    mapTypeId:google.maps.MapTypeId.ROADMAP
+  }
+
+  // Mapa
+  const map = new google.maps.Map(document.getElementById("map"),mapProp);
+  
+    // Janela de informação (info window)
+  const infowindow = new google.maps.InfoWindow({
+    content: "É aqui a WebConference!"
+  })
+
+  // Marcador
+  const marker = new google.maps.Marker({
+    position:porto,
+    map:map,
+    title:"WebConference"
+  })
+
+  // Listener
+  marker.addListener('click', function() {
+    infowindow.open(map, marker);
+  })
+
+} 
