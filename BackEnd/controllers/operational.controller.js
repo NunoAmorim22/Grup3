f2e45c51d0ad99e0cdd753d92bae9a8df184eeea
onlymr <a94122@alunos.uminho.pt>
2021-01-23 21:15:42 +0000

    andremosnter

--- BackEnd/controllers/operational.controller.js
+++ BackEnd/controllers/operational.controller.js
@@ -220,8 +220,8 @@ function deleteOpByAdmin(req , res) {
         console.log(query.sql);
         if (!err){
             res.status(jsonMessages.db.successDeleteU.status).send (jsonMessages.db.successDeleteU);
-            
-            const query = connect.con.query("DELETE FROM Candidate WHERE id_candidate=(SELECT id_candidate FROM Operational WHERE id_operational=LAST_INSERT_ID())", post , function (err, rows, fields){
+
+            const query = connect.con.query("DELETE FROM Candidate WHERE id_candidate=(SELECT id_candidate FROM Operational WHERE id_operational=LAST_INSERT_ID())" , function (err, rows, fields){
                 console.log(query.sql);
                 if (!err){
                     res.status(jsonMessages.db.successDeleteU.status).send (jsonMessages.db.successDeleteU);
