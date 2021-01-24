7a25db1715d26b43c9ca182df3e6bf7187f3a593
onlymr <a94122@alunos.uminho.pt>
2021-01-24 02:08:17 +0000

    andremosnter

--- BackEnd/controllers/operational.controller.js
+++ BackEnd/controllers/operational.controller.js
@@ -180,11 +180,7 @@ function EditOperationalData(req,res){
                     else
                         res.status(jsonMessages.db.dbError.status).send(jsonMessages.db.dbError);
                     }
-                    var post=[email, password, idOperational];
-                    const query = connect.con.query ('UPDATE User_old SET email=?, password=? WHERE id=(SELECT id_user FROM Operational WHERE id_operational=?)',post, function(err,rows, fields){
-                        console.log(query.sql);
-                        res.status(jsonMessages.db.successInsert.status).send (jsonMessages.db.successInsert);
-                        });
+                    
     });
     }    
 }
@@ -486,6 +482,12 @@ function changePassword(req,res){
         const query = connect.con.query("UPDATE users AS us INNER JOIN User_old AS uso ON us.email=uso.email SET us.password=? WHERE us.email=?", post, function (err, rows, fields){
             console.log(query.sql);
             if (!err){
+                var post=[email, password, idOperational];
+                    const query = connect.con.query ('UPDATE User_old SET email=?, password=? WHERE id=(SELECT id_user FROM Operational WHERE id_operational=?)',post, function(err,rows, fields){
+                        console.log(query.sql);
+                        res.status(jsonMessages.db.successInsert.status).send (jsonMessages.db.successInsert);
+                        });
+                        
                 res.status(jsonMessages.db.successUpdate.status).send(jsonMessages.db.successUpdate);
                     }
                     else{
